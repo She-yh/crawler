63dfd1a50dad7255b5865feedd4b92fd905fea96
She-yh <965598930@qq.com>
2023-07-23 14:59:58 +0800

    update main branch

--- src/components/echartsTable.vue
+++ src/components/echartsTable.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="echartsTable">
-sss
+    sss
   </div>
 </template>
 
@@ -11,8 +11,8 @@ import { TooltipComponent, VisualMapComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import { Bar3DChart } from 'echarts-gl/charts';
 import { Grid3DComponent } from 'echarts-gl/components';
-import { onMounted } from 'vue-demi'
-import {option} from './options'
+import { onMounted } from 'vue'
+import { option } from './options'
 echarts.use([
   TooltipComponent,
   VisualMapComponent,
@@ -21,22 +21,22 @@ echarts.use([
   CanvasRenderer
 ]);
 export default {
-    setup(){
-        onMounted(() => {
-            var myChart = echarts.init(document.getElementById('echartsTable'))
-            option && myChart.setOption(option);
-        })
-    }
+  setup() {
+    onMounted(() => {
+      var myChart = echarts.init(document.getElementById('echartsTable'))
+      option && myChart.setOption(option);
+    })
+  }
 }
 </script>
 
 <style>
-#echartsTable{
-    top: 0;
-    position: fixed !important;
-    height: 30vh;
-    max-width: 980px;
-    min-width: 300px;
-    width: 100%;
+#echartsTable {
+  top: 0;
+  position: fixed !important;
+  height: 30vh;
+  max-width: 980px;
+  min-width: 300px;
+  width: 100%;
 }
 </style>
